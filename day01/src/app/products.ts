436d5d4ec3f07e474652ddd8c919e430bf19cd87
hasansin <63632399+hasansin@users.noreply.github.com>
2021-08-06 11:58:36 +0530

    Data passing from child

--- day01/src/app/products.ts
+++ day01/src/app/products.ts
@@ -1,4 +1,4 @@
-export interface Products {
+export interface Product {
   id: number;
   name: string;
   price: number;
@@ -6,22 +6,22 @@ export interface Products {
 }
 
 export const products = [
-    {
-      id: 1,
-      name: 'Phone XL',
-      price: 799,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      id: 2,
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras'
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: ''
-    }
-  ];
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+  },
+];
