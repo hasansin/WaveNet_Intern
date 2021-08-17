e7ac7414a711b2af6452e1bb98a12d8cd30c172b
hasansin <63632399+hasansin@users.noreply.github.com>
2021-08-17 11:40:22 +0530

    Lab DB1 completed

--- ng2-training-master/src/app/license-plate/license-plate.component.ts
+++ ng2-training-master/src/app/license-plate/license-plate.component.ts
@@ -1,18 +1,21 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {LicensePlate} from '../license-plate';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { LicensePlate } from "../license-plate";
 
 @Component({
-  selector: 'app-license-plate',
-  templateUrl: './license-plate.component.html',
-  styleUrls: ['./license-plate.component.css']
+  selector: "app-license-plate",
+  templateUrl: "./license-plate.component.html",
+  styleUrls: ["./license-plate.component.css"],
 })
 export class LicensePlateComponent {
-
   @Input()
   plate: LicensePlate;
 
   @Input()
   buttonText: string;
 
-  constructor() { }
+  constructor() {}
+
+  buttonClicked() {
+    alert("plate added to cart!!");
+  }
 }
